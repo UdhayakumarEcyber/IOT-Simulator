58027c8e5fd47df20627e7f071f231b4707c1446
UdhayakumarEcyber <42633455+UdhayakumarEcyber@users.noreply.github.com>
2018-08-23 16:07:23 +0530

    Add files via upload

---
+++ bottombar.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+export interface IBottomBarProps {
+    
+}
+export interface IBottomBarState {
+    
+}
+export class BottomBar extends React.Component<IBottomBarProps,IBottomBarState> {
+    constructor(props:IBottomBarProps) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+       // return <div className='bottombar' />;
+        return (
+            <div className="bottombar">
+                <div className="footer">			
+                    <div className="footer_lft">				   
+                        <div className="play-btn"><a href="#"><img src="images/play-btn.svg" /></a></div>
+                        <div className="play-duration"><span></span></div>
+                        <div className="tick"><a href="#"><img src="images/Tick.svg"/></a></div>				  
+                    </div>
+                </div> 
+            </div>
+          );
+    }
+}
